ddae4bd46d48a607604d311bdf69af1b079528bb
Robin <Code6226@gmail.com>
2013-12-28 14:13:09 -0500

    Added server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// Server
+var _ = require('lodash');
+var app = require('express')()
+    , http = require('http')
+    , server = http.createServer(app)
+    , io = require('socket.io').listen(server)
+
+server.listen(8060);
+
+io.configure(function(){ //https://github.com/LearnBoost/Socket.IO/wiki/Configuring-Socket.IO
+    io.set('log level', 1);
+});
+
+io.sockets.on('connection', function(socket){
+    console.log('new connection just now!')
+    socket.on('sendChat', function (data) {
+        console.log(data.username +' spoke!')
+        io.sockets.emit('userSpeaks', data);
+    });
+});
