fb691a4ed07969e6a258cff683bfdae448da95e0
Robin <Code6226@gmail.com>
2013-12-28 14:12:33 -0500

    Almost a chat

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,34 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
+angular.module('myApp.controllers', [])
+  .controller('MyCtrl2', [function($scope) {
 
   }]);
+
+var MainController = function($scope){
+}
+
+var HomeController = function($scope){
+    $scope.dog = 'cat';
+}
+
+
+var ChatController = function($scope, socket){
+    $scope.enteredText = '';
+    $scope.chatLines = [
+        {username: 'George', text: 'Hey'}
+    ];
+
+    socket.on('userSpeaks', function (data) {
+        $scope.$apply(function(){
+            console.log(data);
+            $scope.chatLines.push(data);
+        });
+    });
+
+    $scope.sendChat = function(){
+        socket.emit('sendChat',{username: 'User#256', text: $scope.enteredText})
+        $scope.enteredText = '';
+    }
+}
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,10 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+    factory('socket',function($location){
+        var webSocketAddress = 'http://'+window.location.hostname+':8060';
+        var socket = io.connect(webSocketAddress, {
+            reconnect: false
+        });
+        return socket;
+    });
